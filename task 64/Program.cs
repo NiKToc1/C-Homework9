2b5b9fdfbe37d733fd36a6304dc704c7174b9eba
NiKToc <niktoc20@gmail.com>
2023-06-27 20:00:29 +0500

    task 64 & 66

---
+++ task 64/Program.cs
@@ -0,0 +1,14 @@
+﻿int printNumbers(int a, int b)
+{
+    if (a == b)
+        return a;
+    Console.Write($"{printNumbers(a, b + 1)}, ");
+    return b;
+}
+
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int m = 1;
+Console.WriteLine(printNumbers(n, m));
---
+++ task 66/Program.cs
@@ -0,0 +1,13 @@
+﻿int printNumbers(int a, int b)
+{
+    if (a == b)
+        return b;
+    return (b + printNumbers(a, b - 1));
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(printNumbers(m, n));
