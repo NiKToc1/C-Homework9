c16c349fc4c076774a03e8b82a9a35de9dff0a71
NiKToc <niktoc20@gmail.com>
2023-06-27 20:07:17 +0500

    task 68

---
+++ task 68/Program.cs
@@ -0,0 +1,15 @@
+﻿int AkkermanFunc(int a, int b)
+{
+    if (a == 0)
+        return b + 1;
+    if (a > 0 && b == 0)
+        return AkkermanFunc(a - 1, 1);
+    else return AkkermanFunc(a - 1, AkkermanFunc(a, b - 1));
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"A(m, n) = {AkkermanFunc(m, n)}");
